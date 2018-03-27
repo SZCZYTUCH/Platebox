bb832f64c18c8ff77fcafc7f6b5f09a6c3ce7cfc
SZCZYTUCH <>
2018-03-27 12:58:59 +0200

    more code

--- app/scripts/services/mapService.js
+++ app/scripts/services/mapService.js
@@ -28,9 +28,9 @@
                 }
             };
             
-            self.tries = 2500;
+            self.tries = 40;
             self.roomExtraSize = 1;
-            self.windingPercent = 100;
+            self.windingPercent = 50;
             
             self.generateRooms = function (specs) {
                 for (var i = 0; i < self.tries; i++) {
@@ -115,42 +115,19 @@
             };
             
             self.canThisCellBeNextCandidate = function(cell, direction, specs){
-                //candidate cell can not be room wall, border, can be another corridor tho
-                
+                //candidate cell can not be room wall, border, can be another corridor tho 
                 var cellCopy = angular.copy(cell);
-                
                 var key = Object.keys(direction)[0];
-                
                 cellCopy[key] = cellCopy[key] + direction[key];
                 
-                //console.log(cellCopy, direction);
-                
-                //console.log(specs);
-                
-                //todo optimalize
-                
                 //if out of bounds can not be candidate
-                if (cellCopy.x < 0 || cellCopy.y < 0 || cellCopy.x > specs.w || cellCopy.y > specs.h){
-                    //console.log(cellCopy.x < 0 , cellCopy.y < 0 , cellCopy.x > specs.w , cellCopy.y > specs.h);
-                    return false;
-                }
-                // if filled - can not be candidate
-                else if (self.grid['G'+(cellCopy.x)+'_'+(cellCopy.y)].filled){
-                    //console.log('G'+(cellCopy.x)+'_'+(cellCopy.y)+' is '+ self.grid['G'+(cellCopy.x)+'_'+(cellCopy.y)].filled);
-                    return false;
-                }
-                else {
-                    return true;
-                }
-                
-                
-                
-                
-                
-                //todo
+                return  (!(cellCopy.x < 0 || cellCopy.y < 0 || cellCopy.x > specs.w || cellCopy.y > specs.h)) && 
+                //if filled - can not be candidate
+                        (!(self.grid['G'+(cellCopy.x)+'_'+(cellCopy.y)].filled));
+
             };
             
-            //todo
+
             self.generateCorridor = function (startingPoint, specs) {
                 //console.log(startingPoint);
                 var Directions = [{x:-2}, {x:2}, {y:-2}, {y:2}];
@@ -214,6 +191,86 @@
                 }
 
             };
+            
+            self.createDoorConnections = function () {
+
+
+                // Find all of the tiles that can connect two (or more) regions.
+                //var connectorRegions = <Vec, Set<int>>{};
+                for (var pos in bounds.inflate(-1)) {
+                    // Can't already be part of a region.
+                    if (getTile(pos) != Tiles.wall)
+                        continue;
+
+                    //var regions = new Set<int>();
+                    for (var dir in Direction.CARDINAL) {
+                        var region = _regions[pos + dir];
+                        if (region != null)
+                            regions.add(region);
+                    }
+
+                    if (regions.length < 2)
+                        continue;
+
+                    connectorRegions[pos] = regions;
+                }
+
+                var connectors = connectorRegions.keys.toList();
+
+                // Keep track of which regions have been merged. This maps an original
+                // region index to the one it has been merged to.
+                var merged = {};
+                //var openRegions = new Set<int>();
+                for (var i = 0; i <= _currentRegion; i++) {
+                    merged[i] = i;
+                    openRegions.add(i);
+                }
+
+                // Keep connecting regions until we're down to one.
+                while (openRegions.length > 1) {
+                    var connector = rng.item(connectors);
+
+                    // Carve the connection.
+                    _addJunction(connector);
+
+                    // Merge the connected regions. We'll pick one region (arbitrarily) and
+                    // map all of the other regions to its index.
+                    var regions = connectorRegions[connector]
+                            .map((region) => merged[region]);
+                    var dest = regions.first;
+                    var sources = regions.skip(1).toList();
+
+                    // Merge all of the affected regions. We have to look at *all* of the
+                    // regions because other regions may have previously been merged with
+                    // some of the ones we're merging now.
+                    for (var i = 0; i <= _currentRegion; i++) {
+                        if (sources.contains(merged[i])) {
+                            merged[i] = dest;
+                        }
+                    }
+
+                    // The sources are no longer in use.
+                    openRegions.removeAll(sources);
+
+                    // Remove any connectors that aren't needed anymore.
+                    connectors.removeWhere((pos), {
+                        // Don't allow connectors right next to each other.
+                        //if (connector - pos < 2) return true;
+
+                        // If the connector no long spans different regions, we don't need it.
+                        //var regions = connectorRegions[pos].map((region) => merged[region])
+                        //    .toSet();
+
+                        //if (regions.length > 1) return false;
+
+                        // This connecter isn't needed, but connect it occasionally so that the
+                        // dungeon isn't singly-connected.
+                        //if (rng.oneIn(extraConnectorChance)) _addJunction(pos);
+
+                        //return true;
+                    });
+                }
+            }
             
             self.getMap = function (specs) {
                 self.fillGrid(specs);
