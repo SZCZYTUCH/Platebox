0fe17fdce39b9c37e5595454d93efc20c7a1c44a
SZCZYTUCH <15past7@gmail.com>
2018-06-08 12:20:18 +0200

    draw service functions

--- app/scripts/services/drawService.js
+++ app/scripts/services/drawService.js
@@ -8,9 +8,17 @@
                     
             self.drawMap = function(map, specs){
                 
-                
-                
-                
+                _.forEach(map.grid, function (cell, key) {
+                    
+                    var x0, y0;
+                    x0 = (cell.y * specs.size)+1;
+                    y0 = (cell.x * specs.size)+1;
+                    
+                    self.mapShape.graphics.beginFill(cell.background);
+                    self.mapShape.graphics.rect(x0, y0, specs.size-1, specs.size-1);      
+                    self.mapShape.graphics.endFill();     
+
+                });
                 
                 return self.mapShape;
             };
